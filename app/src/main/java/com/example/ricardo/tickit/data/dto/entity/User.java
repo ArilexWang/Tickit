cd87351b2c44e2dfaa96f592d4ad175174f7e850
ArilexWang <383234436@qq.com>
2017-11-22 14:46:58 +0800

    signup api2

--- app/src/main/java/com/example/ricardo/tickit/data/dto/entity/User.java
+++ app/src/main/java/com/example/ricardo/tickit/data/dto/entity/User.java
@@ -11,29 +11,18 @@ import org.greenrobot.greendao.annotation.Generated;
 public class User {
     @Id
     private Long id;
-    private String password;
-    private String userName;
+    private String nickName;
     private String realName;
-    private String avatarPath;
-    private long mobileNumber;
-    private String email;
-    private Boolean isSuperuser = false;
-    private int level = 1;
-    private int experience = 0;
-    @Generated(hash = 276614086)
-    public User(Long id, String password, String userName, String realName,
-            String avatarPath, long mobileNumber, String email, Boolean isSuperuser,
-            int level, int experience) {
+    private Long mobileNumber;
+    private String password;
+    @Generated(hash = 1115104175)
+    public User(Long id, String nickName, String realName, Long mobileNumber,
+            String password) {
         this.id = id;
-        this.password = password;
-        this.userName = userName;
+        this.nickName = nickName;
         this.realName = realName;
-        this.avatarPath = avatarPath;
         this.mobileNumber = mobileNumber;
-        this.email = email;
-        this.isSuperuser = isSuperuser;
-        this.level = level;
-        this.experience = experience;
+        this.password = password;
     }
     @Generated(hash = 586692638)
     public User() {
@@ -44,17 +33,11 @@ public class User {
     public void setId(Long id) {
         this.id = id;
     }
-    public String getPassword() {
-        return this.password;
+    public String getNickName() {
+        return this.nickName;
     }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public String getUserName() {
-        return this.userName;
-    }
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
     }
     public String getRealName() {
         return this.realName;
@@ -62,40 +45,16 @@ public class User {
     public void setRealName(String realName) {
         this.realName = realName;
     }
-    public String getAvatarPath() {
-        return this.avatarPath;
-    }
-    public void setAvatarPath(String avatarPath) {
-        this.avatarPath = avatarPath;
-    }
-    public long getMobileNumber() {
+    public Long getMobileNumber() {
         return this.mobileNumber;
     }
-    public void setMobileNumber(long mobileNumber) {
+    public void setMobileNumber(Long mobileNumber) {
         this.mobileNumber = mobileNumber;
     }
-    public String getEmail() {
-        return this.email;
+    public String getPassword() {
+        return this.password;
     }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public Boolean getIsSuperuser() {
-        return this.isSuperuser;
-    }
-    public void setIsSuperuser(Boolean isSuperuser) {
-        this.isSuperuser = isSuperuser;
-    }
-    public int getLevel() {
-        return this.level;
-    }
-    public void setLevel(int level) {
-        this.level = level;
-    }
-    public int getExperience() {
-        return this.experience;
-    }
-    public void setExperience(int experience) {
-        this.experience = experience;
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
--- app/src/main/java/com/example/ricardo/tickit/greendao/gen/UserDao.java
+++ app/src/main/java/com/example/ricardo/tickit/greendao/gen/UserDao.java
@@ -25,15 +25,10 @@ public class UserDao extends AbstractDao<User, Long> {
      */
     public static class Properties {
         public final static Property Id = new Property(0, Long.class, "id", true, "_id");
-        public final static Property Password = new Property(1, String.class, "password", false, "PASSWORD");
-        public final static Property UserName = new Property(2, String.class, "userName", false, "USER_NAME");
-        public final static Property RealName = new Property(3, String.class, "realName", false, "REAL_NAME");
-        public final static Property AvatarPath = new Property(4, String.class, "avatarPath", false, "AVATAR_PATH");
-        public final static Property MobileNumber = new Property(5, long.class, "mobileNumber", false, "MOBILE_NUMBER");
-        public final static Property Email = new Property(6, String.class, "email", false, "EMAIL");
-        public final static Property IsSuperuser = new Property(7, Boolean.class, "isSuperuser", false, "IS_SUPERUSER");
-        public final static Property Level = new Property(8, int.class, "level", false, "LEVEL");
-        public final static Property Experience = new Property(9, int.class, "experience", false, "EXPERIENCE");
+        public final static Property NickName = new Property(1, String.class, "nickName", false, "NICK_NAME");
+        public final static Property RealName = new Property(2, String.class, "realName", false, "REAL_NAME");
+        public final static Property MobileNumber = new Property(3, Long.class, "mobileNumber", false, "MOBILE_NUMBER");
+        public final static Property Password = new Property(4, String.class, "password", false, "PASSWORD");
     }
 
 
@@ -50,15 +45,10 @@ public class UserDao extends AbstractDao<User, Long> {
         String constraint = ifNotExists? "IF NOT EXISTS ": "";
         db.execSQL("CREATE TABLE " + constraint + "\"USER\" (" + //
                 "\"_id\" INTEGER PRIMARY KEY ," + // 0: id
-                "\"PASSWORD\" TEXT," + // 1: password
-                "\"USER_NAME\" TEXT," + // 2: userName
-                "\"REAL_NAME\" TEXT," + // 3: realName
-                "\"AVATAR_PATH\" TEXT," + // 4: avatarPath
-                "\"MOBILE_NUMBER\" INTEGER NOT NULL ," + // 5: mobileNumber
-                "\"EMAIL\" TEXT," + // 6: email
-                "\"IS_SUPERUSER\" INTEGER," + // 7: isSuperuser
-                "\"LEVEL\" INTEGER NOT NULL ," + // 8: level
-                "\"EXPERIENCE\" INTEGER NOT NULL );"); // 9: experience
+                "\"NICK_NAME\" TEXT," + // 1: nickName
+                "\"REAL_NAME\" TEXT," + // 2: realName
+                "\"MOBILE_NUMBER\" INTEGER," + // 3: mobileNumber
+                "\"PASSWORD\" TEXT);"); // 4: password
     }
 
     /** Drops the underlying database table. */
@@ -76,38 +66,25 @@ public class UserDao extends AbstractDao<User, Long> {
             stmt.bindLong(1, id);
         }
  
-        String password = entity.getPassword();
-        if (password != null) {
-            stmt.bindString(2, password);
-        }
- 
-        String userName = entity.getUserName();
-        if (userName != null) {
-            stmt.bindString(3, userName);
+        String nickName = entity.getNickName();
+        if (nickName != null) {
+            stmt.bindString(2, nickName);
         }
  
         String realName = entity.getRealName();
         if (realName != null) {
-            stmt.bindString(4, realName);
+            stmt.bindString(3, realName);
         }
  
-        String avatarPath = entity.getAvatarPath();
-        if (avatarPath != null) {
-            stmt.bindString(5, avatarPath);
-        }
-        stmt.bindLong(6, entity.getMobileNumber());
- 
-        String email = entity.getEmail();
-        if (email != null) {
-            stmt.bindString(7, email);
+        Long mobileNumber = entity.getMobileNumber();
+        if (mobileNumber != null) {
+            stmt.bindLong(4, mobileNumber);
         }
  
-        Boolean isSuperuser = entity.getIsSuperuser();
-        if (isSuperuser != null) {
-            stmt.bindLong(8, isSuperuser ? 1L: 0L);
+        String password = entity.getPassword();
+        if (password != null) {
+            stmt.bindString(5, password);
         }
-        stmt.bindLong(9, entity.getLevel());
-        stmt.bindLong(10, entity.getExperience());
     }
 
     @Override
@@ -119,38 +96,25 @@ public class UserDao extends AbstractDao<User, Long> {
             stmt.bindLong(1, id);
         }
  
-        String password = entity.getPassword();
-        if (password != null) {
-            stmt.bindString(2, password);
-        }
- 
-        String userName = entity.getUserName();
-        if (userName != null) {
-            stmt.bindString(3, userName);
+        String nickName = entity.getNickName();
+        if (nickName != null) {
+            stmt.bindString(2, nickName);
         }
  
         String realName = entity.getRealName();
         if (realName != null) {
-            stmt.bindString(4, realName);
+            stmt.bindString(3, realName);
         }
  
-        String avatarPath = entity.getAvatarPath();
-        if (avatarPath != null) {
-            stmt.bindString(5, avatarPath);
-        }
-        stmt.bindLong(6, entity.getMobileNumber());
- 
-        String email = entity.getEmail();
-        if (email != null) {
-            stmt.bindString(7, email);
+        Long mobileNumber = entity.getMobileNumber();
+        if (mobileNumber != null) {
+            stmt.bindLong(4, mobileNumber);
         }
  
-        Boolean isSuperuser = entity.getIsSuperuser();
-        if (isSuperuser != null) {
-            stmt.bindLong(8, isSuperuser ? 1L: 0L);
+        String password = entity.getPassword();
+        if (password != null) {
+            stmt.bindString(5, password);
         }
-        stmt.bindLong(9, entity.getLevel());
-        stmt.bindLong(10, entity.getExperience());
     }
 
     @Override
@@ -162,15 +126,10 @@ public class UserDao extends AbstractDao<User, Long> {
     public User readEntity(Cursor cursor, int offset) {
         User entity = new User( //
             cursor.isNull(offset + 0) ? null : cursor.getLong(offset + 0), // id
-            cursor.isNull(offset + 1) ? null : cursor.getString(offset + 1), // password
-            cursor.isNull(offset + 2) ? null : cursor.getString(offset + 2), // userName
-            cursor.isNull(offset + 3) ? null : cursor.getString(offset + 3), // realName
-            cursor.isNull(offset + 4) ? null : cursor.getString(offset + 4), // avatarPath
-            cursor.getLong(offset + 5), // mobileNumber
-            cursor.isNull(offset + 6) ? null : cursor.getString(offset + 6), // email
-            cursor.isNull(offset + 7) ? null : cursor.getShort(offset + 7) != 0, // isSuperuser
-            cursor.getInt(offset + 8), // level
-            cursor.getInt(offset + 9) // experience
+            cursor.isNull(offset + 1) ? null : cursor.getString(offset + 1), // nickName
+            cursor.isNull(offset + 2) ? null : cursor.getString(offset + 2), // realName
+            cursor.isNull(offset + 3) ? null : cursor.getLong(offset + 3), // mobileNumber
+            cursor.isNull(offset + 4) ? null : cursor.getString(offset + 4) // password
         );
         return entity;
     }
@@ -178,15 +137,10 @@ public class UserDao extends AbstractDao<User, Long> {
     @Override
     public void readEntity(Cursor cursor, User entity, int offset) {
         entity.setId(cursor.isNull(offset + 0) ? null : cursor.getLong(offset + 0));
-        entity.setPassword(cursor.isNull(offset + 1) ? null : cursor.getString(offset + 1));
-        entity.setUserName(cursor.isNull(offset + 2) ? null : cursor.getString(offset + 2));
-        entity.setRealName(cursor.isNull(offset + 3) ? null : cursor.getString(offset + 3));
-        entity.setAvatarPath(cursor.isNull(offset + 4) ? null : cursor.getString(offset + 4));
-        entity.setMobileNumber(cursor.getLong(offset + 5));
-        entity.setEmail(cursor.isNull(offset + 6) ? null : cursor.getString(offset + 6));
-        entity.setIsSuperuser(cursor.isNull(offset + 7) ? null : cursor.getShort(offset + 7) != 0);
-        entity.setLevel(cursor.getInt(offset + 8));
-        entity.setExperience(cursor.getInt(offset + 9));
+        entity.setNickName(cursor.isNull(offset + 1) ? null : cursor.getString(offset + 1));
+        entity.setRealName(cursor.isNull(offset + 2) ? null : cursor.getString(offset + 2));
+        entity.setMobileNumber(cursor.isNull(offset + 3) ? null : cursor.getLong(offset + 3));
+        entity.setPassword(cursor.isNull(offset + 4) ? null : cursor.getString(offset + 4));
      }
     
     @Override
