5dc1d80f04f4f688798e69e1d0f036b44b6a83b9
ArilexWang <383234436@qq.com>
2017-11-30 22:24:07 +0800

    signup with greendao

--- app/src/main/java/com/example/ricardo/tickit/data/network/api/SigninApi.kt
+++ app/src/main/java/com/example/ricardo/tickit/data/network/api/SigninApi.kt
@@ -6,7 +6,6 @@ import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
-
 interface SigninApi{
     @FormUrlEncoded
     @POST("userSignIn/")
--- app/src/main/java/com/example/ricardo/tickit/greendao/gen/GDUserDao.java
+++ app/src/main/java/com/example/ricardo/tickit/greendao/gen/GDUserDao.java
@@ -15,7 +15,7 @@ import com.example.ricardo.tickit.data.entity.GDUser;
 /** 
  * DAO for table "GDUSER".
 */
-public class GDUserDao extends AbstractDao<GDUser, Long> {
+public class GDUserDao extends AbstractDao<GDUser, String> {
 
     public static final String TABLENAME = "GDUSER";
 
@@ -24,11 +24,12 @@ public class GDUserDao extends AbstractDao<GDUser, Long> {
      * Can be used for QueryBuilder and for referencing column names.
      */
     public static class Properties {
-        public final static Property Id = new Property(0, Long.class, "id", true, "_id");
+        public final static Property Id = new Property(0, String.class, "id", true, "ID");
         public final static Property NickName = new Property(1, String.class, "nickName", false, "NICK_NAME");
         public final static Property RealName = new Property(2, String.class, "realName", false, "REAL_NAME");
-        public final static Property MobileNumber = new Property(3, Long.class, "mobileNumber", false, "MOBILE_NUMBER");
+        public final static Property MobileNumber = new Property(3, String.class, "mobileNumber", false, "MOBILE_NUMBER");
         public final static Property Password = new Property(4, String.class, "password", false, "PASSWORD");
+        public final static Property Avatar = new Property(5, String.class, "avatar", false, "AVATAR");
     }
 
 
@@ -44,11 +45,12 @@ public class GDUserDao extends AbstractDao<GDUser, Long> {
     public static void createTable(Database db, boolean ifNotExists) {
         String constraint = ifNotExists? "IF NOT EXISTS ": "";
         db.execSQL("CREATE TABLE " + constraint + "\"GDUSER\" (" + //
-                "\"_id\" INTEGER PRIMARY KEY ," + // 0: id
+                "\"ID\" TEXT PRIMARY KEY NOT NULL ," + // 0: id
                 "\"NICK_NAME\" TEXT," + // 1: nickName
                 "\"REAL_NAME\" TEXT," + // 2: realName
-                "\"MOBILE_NUMBER\" INTEGER," + // 3: mobileNumber
-                "\"PASSWORD\" TEXT);"); // 4: password
+                "\"MOBILE_NUMBER\" TEXT," + // 3: mobileNumber
+                "\"PASSWORD\" TEXT," + // 4: password
+                "\"AVATAR\" TEXT);"); // 5: avatar
     }
 
     /** Drops the underlying database table. */
@@ -61,9 +63,9 @@ public class GDUserDao extends AbstractDao<GDUser, Long> {
     protected final void bindValues(DatabaseStatement stmt, GDUser entity) {
         stmt.clearBindings();
  
-        Long id = entity.getId();
+        String id = entity.getId();
         if (id != null) {
-            stmt.bindLong(1, id);
+            stmt.bindString(1, id);
         }
  
         String nickName = entity.getNickName();
@@ -76,14 +78,19 @@ public class GDUserDao extends AbstractDao<GDUser, Long> {
             stmt.bindString(3, realName);
         }
  
-        Long mobileNumber = entity.getMobileNumber();
+        String mobileNumber = entity.getMobileNumber();
         if (mobileNumber != null) {
-            stmt.bindLong(4, mobileNumber);
+            stmt.bindString(4, mobileNumber);
         }
  
         String password = entity.getPassword();
         if (password != null) {
             stmt.bindString(5, password);
+        }
+ 
+        String avatar = entity.getAvatar();
+        if (avatar != null) {
+            stmt.bindString(6, avatar);
         }
     }
 
@@ -91,9 +98,9 @@ public class GDUserDao extends AbstractDao<GDUser, Long> {
     protected final void bindValues(SQLiteStatement stmt, GDUser entity) {
         stmt.clearBindings();
  
-        Long id = entity.getId();
+        String id = entity.getId();
         if (id != null) {
-            stmt.bindLong(1, id);
+            stmt.bindString(1, id);
         }
  
         String nickName = entity.getNickName();
@@ -106,51 +113,57 @@ public class GDUserDao extends AbstractDao<GDUser, Long> {
             stmt.bindString(3, realName);
         }
  
-        Long mobileNumber = entity.getMobileNumber();
+        String mobileNumber = entity.getMobileNumber();
         if (mobileNumber != null) {
-            stmt.bindLong(4, mobileNumber);
+            stmt.bindString(4, mobileNumber);
         }
  
         String password = entity.getPassword();
         if (password != null) {
             stmt.bindString(5, password);
         }
+ 
+        String avatar = entity.getAvatar();
+        if (avatar != null) {
+            stmt.bindString(6, avatar);
+        }
     }
 
     @Override
-    public Long readKey(Cursor cursor, int offset) {
-        return cursor.isNull(offset + 0) ? null : cursor.getLong(offset + 0);
+    public String readKey(Cursor cursor, int offset) {
+        return cursor.isNull(offset + 0) ? null : cursor.getString(offset + 0);
     }    
 
     @Override
     public GDUser readEntity(Cursor cursor, int offset) {
         GDUser entity = new GDUser( //
-            cursor.isNull(offset + 0) ? null : cursor.getLong(offset + 0), // id
+            cursor.isNull(offset + 0) ? null : cursor.getString(offset + 0), // id
             cursor.isNull(offset + 1) ? null : cursor.getString(offset + 1), // nickName
             cursor.isNull(offset + 2) ? null : cursor.getString(offset + 2), // realName
-            cursor.isNull(offset + 3) ? null : cursor.getLong(offset + 3), // mobileNumber
-            cursor.isNull(offset + 4) ? null : cursor.getString(offset + 4) // password
+            cursor.isNull(offset + 3) ? null : cursor.getString(offset + 3), // mobileNumber
+            cursor.isNull(offset + 4) ? null : cursor.getString(offset + 4), // password
+            cursor.isNull(offset + 5) ? null : cursor.getString(offset + 5) // avatar
         );
         return entity;
     }
      
     @Override
     public void readEntity(Cursor cursor, GDUser entity, int offset) {
-        entity.setId(cursor.isNull(offset + 0) ? null : cursor.getLong(offset + 0));
+        entity.setId(cursor.isNull(offset + 0) ? null : cursor.getString(offset + 0));
         entity.setNickName(cursor.isNull(offset + 1) ? null : cursor.getString(offset + 1));
         entity.setRealName(cursor.isNull(offset + 2) ? null : cursor.getString(offset + 2));
-        entity.setMobileNumber(cursor.isNull(offset + 3) ? null : cursor.getLong(offset + 3));
+        entity.setMobileNumber(cursor.isNull(offset + 3) ? null : cursor.getString(offset + 3));
         entity.setPassword(cursor.isNull(offset + 4) ? null : cursor.getString(offset + 4));
+        entity.setAvatar(cursor.isNull(offset + 5) ? null : cursor.getString(offset + 5));
      }
     
     @Override
-    protected final Long updateKeyAfterInsert(GDUser entity, long rowId) {
-        entity.setId(rowId);
-        return rowId;
+    protected final String updateKeyAfterInsert(GDUser entity, long rowId) {
+        return entity.getId();
     }
     
     @Override
-    public Long getKey(GDUser entity) {
+    public String getKey(GDUser entity) {
         if(entity != null) {
             return entity.getId();
         } else {
